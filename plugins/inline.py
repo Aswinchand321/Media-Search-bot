13f5184b89f7f34df7899ee6ca884401681d890f
Aswinchand321 <88029112+Aswinchand321@users.noreply.github.com>
2021-07-29 12:27:29 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -83,8 +83,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton(' 🌹Join Group🌹 ', url='https://t.me/Movies_Club_2019'),
+            InlineKeyboardButton(' Help🛠️ ', url='https://t.me/Movies_Club_2019')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
